8819dc8407a35bdaddd77a499a69b6eca6acd547
Edwar J. Guzmán <59851244+edjogu@users.noreply.github.com>
2024-03-08 20:02:02 -0500

    Primer commit

---
+++ GameStore.Api/Dtos/CreateGameDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.Api.Dtos;
+
+public record class CreateGameDto(
+    [Required][MaxLength(50)]string Name,
+    [Required][MaxLength(20)]string Genre,
+    [Range(1, 100)]decimal Price,
+    DateOnly ReleaseDate
+);
+
